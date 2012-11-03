c010de53983909a4bb8d882af3777dc8e37eb56d
Andrei Cristian Petcu <andreicristianpetcu@gmail.com>
2012-11-03 18:29:24 +0200

    mechanize hello world :)

--- scripts/1_clrro_import_download_pages.py
+++ scripts/1_clrro_import_download_pages.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python
-print("test")
+from mechanize import Browser
+
+def get_laws_search_page():  
+  # Navigate to the main search page for laws.
+  browser = Browser()
+  browser.open('http://www.clr.ro/rep_dil_2002/rep.aspx')
+  
+  html = browser.response().get_data().replace("<br/>", "<br />")
+  
+  print("html=" + html)
+  
+#  br.select_form(name='Voturi')
+#  form = br.form
+#
+#  form['drpMonthCal'] = [month]
+
+def main():
+    get_laws_search_page()
+
+
+if  __name__ =='__main__':main()
