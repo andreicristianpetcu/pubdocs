04b601fa947c7102b924cc99e74ce7c70e30bdfa
Andrei Cristian Petcu <andreicristianpetcu@gmail.com>
2012-11-03 23:47:09 +0200

    add tmp dir multi platform support

--- scripts/1_clrro_import_download_pages.py
+++ scripts/1_clrro_import_download_pages.py
@@ -8,37 +8,35 @@ def get_years_with_laws():
     years = [1864, 1865, 1879, 1881, 1887, 1909, 1916, 1918, 1919, 1924, 1925,
              1927, 1929, 1930, 1931, 1933, 1934, 1937, 1938, 1940]
     years += range(1942, 2012)
-    return sorted(years)
+    return years
 
-def generate_laws():  
-  navigate_to_laws_from_year('2006')
-#  navigate_to_laws_from_year('2007')
-#  navigate_to_laws_from_year('2008')
+def generate_laws():
+    for year in get_years_with_laws():
+        fetch_laws_page_from_year('2006')
   
-def navigate_to_laws_from_year(year):  
-  url = get_url_forlaws(year)
-  browser = Browser()
-  browser.open(url)
-  html = browser.response().get_data()
-
-  with open ('clr.ro-laws-' + year + '.html', 'a') as f: 
-      f.write (html)
+def fetch_laws_page_from_year(year):  
+    url = get_ugly_url_for_laws(year)
+    browser = Browser()
+    browser.open(url)
+    html = browser.response().get_data()
+    
+    with open ('clr.ro-laws-' + year + '.html', 'a') as f: 
+        f.write (html)
+    
+    print(html)
   
-  print(html)
-  
-def get_url_forlaws(year):
-  url = "http://www.clr.ro/rep_dil_2002/viewGrid.aspx?Type=Submit&pk1=SELECT%20*%20FROM%20legi%20Where%20legi.cod%20Like%20%27%nr.%/"
-  url += year        
-  url += "%%27%20AND%20NOT%20Isnull(legi.titlul)%20AND%20(legi.Cod_even%3C%3E%277%27%20AND%20legi.Cod_even%3C%3E%278%27%20AND%20legi.Cod_even%3C%3E%279%27%20AND%20(legi.Tip_world=%27%27%20OR%20IsNull(legi.Tip_world))%20AND%20(legi.Nr_world=0%20OR%20IsNull(legi.Nr_world))%20AND%20(legi.An_world=0%20OR%20IsNull(legi.An_world)))%20Order%20by%20Cod_tip%20ASC,%20Cod_an%20ASC,%20Cod_nr%20ASC,%20Cod_even%20ASC%20UNION%20ALL%20SELECT%20*%20FROM%20abrogate%20Where%20abrogate.cod%20Like%20%27%nr.%/"
-  url += year        
-  url += "%%27%20AND%20NOT%20Isnull(abrogate.titlul)%20AND%20(abrogate.Cod_even%3C%3E%277%27%20AND%20abrogate.Cod_even%3C%3E%278%27%20AND%20abrogate.Cod_even%3C%3E%279%27%20AND%20(abrogate.Tip_world=%27%27%20OR%20IsNull(abrogate.Tip_world))%20AND%20(abrogate.Nr_world=0%20OR%20IsNull(abrogate.Nr_world))%20AND%20(abrogate.An_world=0%20OR%20IsNull(abrogate.An_world)))%20Order%20by%20Cod_tip%20ASC,%20Cod_an%20ASC,%20Cod_nr%20ASC,%20Cod_even%20ASC&pk2=RadioPageTot&pk3=&pk4=TOT&pk5="
-  url += year        
-  url += "&pk7=1&pk8=Radio8&pk9=Radio5&pk10=Radio11&pk11=nrdoc_empty1"
-  return url
+def get_ugly_url_for_laws(year):
+    url = "http://www.clr.ro/rep_dil_2002/viewGrid.aspx?Type=Submit&pk1=SELECT%20*%20FROM%20legi%20Where%20legi.cod%20Like%20%27%nr.%/"
+    url += year        
+    url += "%%27%20AND%20NOT%20Isnull(legi.titlul)%20AND%20(legi.Cod_even%3C%3E%277%27%20AND%20legi.Cod_even%3C%3E%278%27%20AND%20legi.Cod_even%3C%3E%279%27%20AND%20(legi.Tip_world=%27%27%20OR%20IsNull(legi.Tip_world))%20AND%20(legi.Nr_world=0%20OR%20IsNull(legi.Nr_world))%20AND%20(legi.An_world=0%20OR%20IsNull(legi.An_world)))%20Order%20by%20Cod_tip%20ASC,%20Cod_an%20ASC,%20Cod_nr%20ASC,%20Cod_even%20ASC%20UNION%20ALL%20SELECT%20*%20FROM%20abrogate%20Where%20abrogate.cod%20Like%20%27%nr.%/"
+    url += year        
+    url += "%%27%20AND%20NOT%20Isnull(abrogate.titlul)%20AND%20(abrogate.Cod_even%3C%3E%277%27%20AND%20abrogate.Cod_even%3C%3E%278%27%20AND%20abrogate.Cod_even%3C%3E%279%27%20AND%20(abrogate.Tip_world=%27%27%20OR%20IsNull(abrogate.Tip_world))%20AND%20(abrogate.Nr_world=0%20OR%20IsNull(abrogate.Nr_world))%20AND%20(abrogate.An_world=0%20OR%20IsNull(abrogate.An_world)))%20Order%20by%20Cod_tip%20ASC,%20Cod_an%20ASC,%20Cod_nr%20ASC,%20Cod_even%20ASC&pk2=RadioPageTot&pk3=&pk4=TOT&pk5="
+    url += year        
+    url += "&pk7=1&pk8=Radio8&pk9=Radio5&pk10=Radio11&pk11=nrdoc_empty1"
+    return url
     
 def main():
 #    generate_laws()
     print(get_years_with_laws())
-
 
 if  __name__ == '__main__':main()
--- scripts/clrro_import.py
+++ scripts/clrro_import.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 import os
 import subprocess
+import platform
 
-print("before")
-print(os.getcwd())
-subprocess.check_call(["./1_clrro_import_download_pages.py", "1"])
-print("after")
+platofrmName = platform.system()
+
+platofrmToTemporaryDirDictionary = {
+  'Linux':'/tmp/legilibere/',
+  'Darwin':'somewhere not in tmp because tmp is deleted on restart',
+  'Windows':'ok, It is your choice :)'}
+
+tmpDir = platofrmToTemporaryDirDictionary.get(platofrmName)
+if tmpDir is None:
+    print("Your platform is " + platofrmName + " and you do not have a temporary dir selected.")
+
+if not os.path.exists(tmpDir):
+    os.makedirs(tmpDir)
+    print('The work directory did not exist so I created It')
+    print(tmpDir)
+    
+subprocess.check_call(["./1_clrro_import_download_pages.py", tmpDir])
