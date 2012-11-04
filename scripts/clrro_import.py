b5a85e207cc8d5bde67b0fddd203e18f42531eaf
Andrei Cristian Petcu <andreicristianpetcu@gmail.com>
2012-11-04 10:59:07 +0200

    added the work dir in instance

--- scripts/clrro_import.py
+++ scripts/clrro_import.py
@@ -3,22 +3,13 @@ import os
 import subprocess
 import platform
 
-platofrmName = platform.system()
+work_dir = "../instance/pages/"
 
-platofrmToTemporaryDirDictionary = {
-  'Linux':'/tmp/legilibere/',
-  'Darwin':'somewhere not in tmp because tmp is deleted on restart',
-  'Windows':'ok, It is your choice :)'}
-
-tmpDir = platofrmToTemporaryDirDictionary.get(platofrmName)
-if tmpDir is None:
-    print("Your platform is " + platofrmName + " and you do not have a temporary dir selected.")
-
-if not os.path.exists(tmpDir):
-    os.makedirs(tmpDir)
+if not os.path.exists(work_dir):
+    os.makedirs(work_dir)
     print('The work directory did not exist so I created it')
-    print(tmpDir)
+    print(work_dir)
 
-clr_ro_dir = os.path.join(tmpDir, 'clr_ro');
+clr_ro_dir = os.path.join(work_dir, 'clr_ro');
 subprocess.check_call(["./1_clrro_fetch_laws_per_year.py", clr_ro_dir])
 subprocess.check_call(["./2_clrro_fetch_individual_laws.py", clr_ro_dir])
